e785f4db84f7fd7c36db77415f76560c18f7c920
irfan önk <57913715+irfanonk@users.noreply.github.com>
2021-12-06 12:34:00 +0300

    env, deploy

--- deploy.js
+++ deploy.js
@@ -3,6 +3,7 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 require("dotenv").config();
 
+// console.log("env", process.env);
 const provider = new HDWalletProvider(
   process.env.MNMC,
   process.env.INFURA_RINKEBY_URL
@@ -16,13 +17,17 @@ const deploy = async () => {
     console.log("Attempting to deploy from account", accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({ data: bytecode })
-      .send({ gas: "1000000", from: accounts[0] });
+      .deploy({
+        data: bytecode,
+        arguments: ["Hi"],
+      })
+      .send({ from: accounts[0], gas: "1000000" });
 
     console.log("Contract deployed to", result.options.address);
     provider.engine.stop();
   } catch (error) {
-    console.log("err", err);
+    console.log("err", error);
+    process.exit(1);
   }
 };
 deploy();
